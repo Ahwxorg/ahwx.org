0783fdd32b2361583fdf965ed0394745f79e57bb
Ahwx <ahwx@ahwx.org>
2023-10-02 16:31:10 +0200

    feat(config.ts): adding a config file so we can easily modify certain stuff

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site.
+
+export const SITE_TITLE = "Ahwx >> Home"
+export const SITE_DESCRIPTION = "My cozy place on the internet"
+export const HOMEPAGE_URL = "https://ahwx.org"
