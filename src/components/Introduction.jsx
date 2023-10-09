3a501540f2aa09c70e82d64a125781d6425d2617
Ahwx <ahwx@ahwx.org>
2023-10-09 04:43:21 +0200

    feat(introduction.jsx): added a React component for the `I'm a <random thing here>` text

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+const textArray = ['technology enthousiast', 'homelabber', 'nerd', 'person', 'student'];
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = { textIdx: 0 };
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.textIdx;
+      this.setState({ textIdx: currentIdx + 1 });
+    }, 3000);
+  }
+
+  componentWillUnmount() { // Corrected method name
+    clearInterval(this.timeout);
+  }
+
+  render() {
+    let introductionText = textArray[this.state.textIdx % textArray.length];
+
+    return (
+      <span>{introductionText}</span>
+    );
+  }
+}
+
+export default Home;
